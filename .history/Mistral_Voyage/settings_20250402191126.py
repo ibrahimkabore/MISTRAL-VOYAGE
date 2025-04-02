64b4c9634e0af5fb54e78de0a9084636a13a5e55
ibrahimkabore <ibrahim.kabore@onma.io>
2025-04-02 19:20:35 +0000

    commint deployer

--- .history/Mistral_Voyage/settings_20250402191126.py
+++ .history/Mistral_Voyage/settings_20250402191126.py
@@ -19,7 +19,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from datetime import timedelta
-import dj_database_url
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
