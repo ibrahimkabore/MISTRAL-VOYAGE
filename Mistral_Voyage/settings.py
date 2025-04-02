7a49befdaf331ffe440aa6a3609d3f8cf16304d7
ibrahimkabore <ibrahim.kabore@onma.io>
2025-04-02 23:51:04 +0000

    Update settings.py

--- Mistral_Voyage/settings.py
+++ Mistral_Voyage/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'mistral-voyage.onrender.com',
+    # Ajoutez d'autres domaines si nécessaire
+]
 
 
 # Application definition
